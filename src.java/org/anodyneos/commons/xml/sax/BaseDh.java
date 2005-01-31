8ec92a48c00c89ed7ea074f7a44d2d3de42e2851
John Vasileff <git@netcc.us>
2005-01-31 08:10:44 +0000

    added logging

--- src.java/org/anodyneos/commons/xml/sax/BaseDh.java
+++ src.java/org/anodyneos/commons/xml/sax/BaseDh.java
@@ -1,13 +1,18 @@
 package org.anodyneos.commons.xml.sax;
 
+import java.text.MessageFormat;
 import java.util.Stack;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.xml.sax.Attributes;
 import org.xml.sax.Locator;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class BaseDh extends DefaultHandler {
+
+    private static final Log log = LogFactory.getLog(BaseDh.class);
 
     protected BaseContext ctx;
     private ElementProcessor topProcessor;
@@ -22,6 +27,11 @@ public class BaseDh extends DefaultHandler {
 
     public void startElement(String uri, String localName, String qName,
             Attributes attributes) throws SAXException {
+
+        if (log.isDebugEnabled()) {
+            log.debug(MessageFormat.format("startElement(''{0}'', ''{1}'', ''{2}'', attributes)",
+                    (Object[]) new String[] { uri, localName, qName }));
+        }
 
         if (! namespaceContextPushed) {
             ctx.getNamespaceSupport().pushContext();
@@ -43,6 +53,11 @@ public class BaseDh extends DefaultHandler {
     }
 
     public void endElement(String uri, String localName, String qName) throws SAXException {
+        if (log.isDebugEnabled()) {
+            log.debug(MessageFormat.format("endElement(''{0}'', ''{1}'', ''{2}'')",
+                    (Object[]) new String[] { uri, localName, qName }));
+        }
+
         ElementProcessor processor = (ElementProcessor) processorStack.pop();
         processor.endElement(uri, localName, qName);
         ctx.getNamespaceSupport().popContext();
@@ -53,14 +68,30 @@ public class BaseDh extends DefaultHandler {
         processor.characters(chars, start, length);
     }
 
+    public void startDocument() throws SAXException {
+        if (log.isDebugEnabled()) {
+            log.debug("startDocument()");
+        }
+    }
+
     public void endDocument() throws SAXException {
+        if (log.isDebugEnabled()) {
+            log.debug("endDocument()");
+        }
     }
 
     public void setDocumentLocator(Locator locator) {
+        if (log.isDebugEnabled()) {
+            log.debug("setDocumentLocator(locator)");
+        }
         ctx.setLocator(locator);
     }
 
     public void startPrefixMapping(java.lang.String prefix, java.lang.String uri) throws SAXException {
+        if (log.isDebugEnabled()) {
+            log.debug(MessageFormat.format("startPrefixMapping(''{0}'', ''{1}'')",
+                    (Object[]) new String[] { prefix, uri }));
+        }
         if (! namespaceContextPushed) {
             ctx.getNamespaceSupport().pushContext();
             namespaceContextPushed = true;
@@ -74,6 +105,10 @@ public class BaseDh extends DefaultHandler {
     }
 
     public void endPrefixMapping(java.lang.String prefix) throws SAXException {
+        if (log.isDebugEnabled()) {
+            log.debug(MessageFormat.format("endPrefixMapping(''{0}'')",
+                    (Object[]) new String[] { prefix }));
+        }
         if (! processorStack.empty()) {
             ElementProcessor processor = (ElementProcessor) processorStack.peek();
             processor.endPrefixMapping(prefix);
